db4567746e389a36a0a9e0663850c7255574a452
marsevilspirit <marsevilspirit@gmail.com>
2025-12-30 19:06:42 +0800

    feat: Add `immutable` attribute support to `OnlineConfig` derive macro fields, preventing runtime modification, and include corresponding tests.

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -5,7 +5,7 @@ use syn::DeriveInput;
 use syn::Fields;
 use syn::parse_macro_input;
 
-#[proc_macro_derive(OnlineConfig)]
+#[proc_macro_derive(OnlineConfig, attributes(online_config))]
 pub fn online_config_derive(input: TokenStream) -> TokenStream {
 	let input = parse_macro_input!(input as DeriveInput);
 	let name = input.ident;
@@ -23,14 +23,34 @@ pub fn online_config_derive(input: TokenStream) -> TokenStream {
 		let field_type = &f.ty;
 		let field_name_str = field_name.as_ref().unwrap().to_string();
 
-		quote! {
-			#field_name_str => {
-				match #field_type::from_str(value) {
-					Ok(v) => {
-						self.#field_name = v;
-						Ok(())
+		let mut is_immutable = false;
+		for attr in &f.attrs {
+			if attr.path().is_ident("online_config") {
+				let _ = attr.parse_nested_meta(|meta| {
+					if meta.path.is_ident("immutable") {
+						is_immutable = true;
 					}
-					Err(_) => Err(format!("Failed to parse value for field '{}'", #field_name_str)),
+					Ok(())
+				});
+			}
+		}
+
+		if is_immutable {
+			quote! {
+				#field_name_str => {
+					Err(format!("Field '{}' is immutable", #field_name_str))
+				}
+			}
+		} else {
+			quote! {
+				#field_name_str => {
+					match #field_type::from_str(value) {
+						Ok(v) => {
+							self.#field_name = v;
+							Ok(())
+						}
+						Err(_) => Err(format!("Failed to parse value for field '{}'", #field_name_str)),
+					}
 				}
 			}
 		}
--- crates/config/tests/test_derive.rs
+++ crates/config/tests/test_derive.rs
@@ -5,8 +5,12 @@ use rstest::rstest;
 
 #[derive(OnlineConfig, Default)]
 struct TestConfig {
+	#[online_config(mutable)]
 	pub addr: String,
+	#[online_config(mutable)]
 	pub port: u16,
+	#[online_config(immutable)]
+	pub id: i32,
 }
 
 #[rstest]
@@ -29,4 +33,13 @@ fn test_set_field_success(#[case] key: &str, #[case] value: &str) {
 fn test_set_field_failure(#[case] key: &str, #[case] value: &str) {
 	let mut conf = TestConfig::default();
 	assert!(conf.set_field(key, value).is_err());
+}
+
+#[test]
+fn test_immutable_field() {
+	let mut conf = TestConfig::default();
+	let res = conf.set_field("id", "123");
+	assert!(res.is_err());
+	assert_eq!(res.unwrap_err(), "Field 'id' is immutable");
+	assert_eq!(conf.id, 0);
 }
