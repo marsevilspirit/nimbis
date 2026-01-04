0a01a8d53e20572dd9f86c2853aa738fc28635b6
marsevilspirit <marsevilspirit@gmail.com>
2026-01-05 03:01:37 +0800

    refactor: simplify command registration by directly importing command types

--- crates/nimbis/src/cmd/table.rs
+++ crates/nimbis/src/cmd/table.rs
@@ -5,9 +5,27 @@ use super::Cmd;
 use super::ConfigGroupCmd;
 use super::DelCmd;
 use super::ExistsCmd;
+use super::ExpireCmd;
 use super::GetCmd;
+use super::HGetAllCmd;
+use super::HGetCmd;
+use super::HLenCmd;
+use super::HMGetCmd;
+use super::HSetCmd;
+use super::LLenCmd;
+use super::LPopCmd;
+use super::LPushCmd;
+use super::LRangeCmd;
 use super::PingCmd;
+use super::RPopCmd;
+use super::RPushCmd;
+use super::SaddCmd;
+use super::ScardCmd;
 use super::SetCmd;
+use super::SismemberCmd;
+use super::SmembersCmd;
+use super::SremCmd;
+use super::TtlCmd;
 
 pub struct CmdTable {
 	inner: HashMap<String, Arc<dyn Cmd>>,
@@ -30,30 +48,27 @@ impl CmdTable {
 		inner.insert("DEL".to_string(), Arc::new(DelCmd::default()));
 		inner.insert("EXISTS".to_string(), Arc::new(ExistsCmd::default()));
 		// hash type cmd
-		inner.insert("HSET".to_string(), Arc::new(super::HSetCmd::new()));
-		inner.insert("HGET".to_string(), Arc::new(super::HGetCmd::new()));
-		inner.insert("HLEN".to_string(), Arc::new(super::HLenCmd::new()));
-		inner.insert("HMGET".to_string(), Arc::new(super::HMGetCmd::new()));
-		inner.insert("HGETALL".to_string(), Arc::new(super::HGetAllCmd::new()));
+		inner.insert("HSET".to_string(), Arc::new(HSetCmd::new()));
+		inner.insert("HGET".to_string(), Arc::new(HGetCmd::new()));
+		inner.insert("HLEN".to_string(), Arc::new(HLenCmd::new()));
+		inner.insert("HMGET".to_string(), Arc::new(HMGetCmd::new()));
+		inner.insert("HGETALL".to_string(), Arc::new(HGetAllCmd::new()));
 		// list type cmd
-		inner.insert("LPUSH".to_string(), Arc::new(super::LPushCmd::new()));
-		inner.insert("RPUSH".to_string(), Arc::new(super::RPushCmd::new()));
-		inner.insert("LPOP".to_string(), Arc::new(super::LPopCmd::new()));
-		inner.insert("RPOP".to_string(), Arc::new(super::RPopCmd::new()));
-		inner.insert("LLEN".to_string(), Arc::new(super::LLenCmd::new()));
-		inner.insert("LRANGE".to_string(), Arc::new(super::LRangeCmd::new()));
+		inner.insert("LPUSH".to_string(), Arc::new(LPushCmd::new()));
+		inner.insert("RPUSH".to_string(), Arc::new(RPushCmd::new()));
+		inner.insert("LPOP".to_string(), Arc::new(LPopCmd::new()));
+		inner.insert("RPOP".to_string(), Arc::new(RPopCmd::new()));
+		inner.insert("LLEN".to_string(), Arc::new(LLenCmd::new()));
+		inner.insert("LRANGE".to_string(), Arc::new(LRangeCmd::new()));
 		// set type cmd
-		inner.insert("SADD".to_string(), Arc::new(super::SaddCmd::new()));
-		inner.insert("SMEMBERS".to_string(), Arc::new(super::SmembersCmd::new()));
-		inner.insert(
-			"SISMEMBER".to_string(),
-			Arc::new(super::SismemberCmd::new()),
-		);
-		inner.insert("SREM".to_string(), Arc::new(super::SremCmd::new()));
-		inner.insert("SCARD".to_string(), Arc::new(super::ScardCmd::new()));
+		inner.insert("SADD".to_string(), Arc::new(SaddCmd::new()));
+		inner.insert("SMEMBERS".to_string(), Arc::new(SmembersCmd::new()));
+		inner.insert("SISMEMBER".to_string(), Arc::new(SismemberCmd::new()));
+		inner.insert("SREM".to_string(), Arc::new(SremCmd::new()));
+		inner.insert("SCARD".to_string(), Arc::new(ScardCmd::new()));
 		// expire type cmd
-		inner.insert("EXPIRE".to_string(), Arc::new(super::ExpireCmd::default()));
-		inner.insert("TTL".to_string(), Arc::new(super::TtlCmd::default()));
+		inner.insert("EXPIRE".to_string(), Arc::new(ExpireCmd::default()));
+		inner.insert("TTL".to_string(), Arc::new(TtlCmd::default()));
 		// config type cmd
 		inner.insert("CONFIG".to_string(), Arc::new(ConfigGroupCmd::new()));
 		Self { inner }
